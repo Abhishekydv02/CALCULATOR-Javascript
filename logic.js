3f86ca8d26fe07da1156c12f50a40bd49e59603b
abhishek <abhishek@abhisheks-MacBook-Air.local>
2022-02-11 21:03:28 +0530

    second commit

--- logic.js
+++ logic.js
@@ -3,6 +3,16 @@ let screen = document.getElementById('screen');
 let buttons= document.querySelectorAll('button');
 let currentValue='';
 screen.value=0;
+
+function userinput(val)
+{
+    if(val=='bs')
+    {
+        currentValue=currentValue.slice(0,currentValue.length-1);
+        screen.value=currentValue;
+    }
+}
+
 for(let item of buttons)
 {
    item.addEventListener('click',(func)=>
@@ -11,7 +21,7 @@ for(let item of buttons)
     // console.log(btntext);
     if(btntext=='AC')
     {
-        currentValue='';
+        currentValue=''; 
         screen.value=0;
     }
     else if(btntext=='X'){
@@ -24,15 +34,24 @@ for(let item of buttons)
         currentValue=eval(currentValue);
       screen.value=eval(currentValue);
     }
-    else if(btntext=='<i class="far fa-backspace"></i>')
+    else if(btntext=='%')
     {
-        screen.value=5555555;
+        screen.value='invalid';
     }
-
-    else{
+    else {
+        let last= currentValue[currentValue.length-1];
+        if((last== '*' || last == '/' || last =='+' || last == '-') &&
+        (btntext== '+' || btntext == '-' || btntext == '*' || btntext == '/'))
+        {
+            currentValue=currentValue.slice(0, currentValue.length-1)+val;
+            screen.value=currentValue;
+        }
+        else 
+        {
         currentValue+=btntext;
         screen.value=currentValue;
+        }
+    }
     }
 
-   })
-}
+   )}
